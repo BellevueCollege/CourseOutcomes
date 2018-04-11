c905391b127625d4fbeceef4db8c338faf1f7208
Nicole Swan <nicole.swan@bellevuecollege.edu>
2018-04-11 11:43:27 -0700

    Committing files to new repo
    - Starting a new repo to get rid of sensitive info committed in previous
    repo. Very limited history, so not much lost there.
    - Also cleaned out compiled code that had been committed and add a
    .gitignore to keep them out.
    - Updated .NET framework to 4.7.1
    - Shout out to original developer Nathan Gudmundson

---
+++ CourseOutcomesWCF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CourseOutcomesWCF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CourseOutcomesWCF")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cfd2b7c5-0536-492f-8e99-9a699f138469")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CourseOutcomesWCF/Service1.svc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using CourseOutcomesWCF.Model;
+using System.Data.Objects;
+
+namespace CourseOutcomesWCF
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CourseOutcomes" in code, svc and config file together.
+	public class CourseOutcomes : IService1
+	{
+		public string GetCourseOutcome(string CourseID)
+		{
+
+
+			string LearningOutcomeOutput = "<ul><li>No outcome found for " + CourseID + "</li></ul>";
+
+			using (CurriculumEntities db = new CurriculumEntities()) 
+			{
+				CourseID = CourseID.Replace('^', '&');
+				var results = db.usp_SELECT_LearningOutcomes(CourseID);
+
+				
+
+				foreach (string outcome in results)
+				{
+					LearningOutcomeOutput = outcome;
+				}
+
+			}
+
+
+			return LearningOutcomeOutput;
+		}
+
+		public string GetData(int data)
+		{
+			return string.Format("You entered: {0}", data);
+		}
+
+
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+	}
+}
